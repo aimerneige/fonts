e722719f42400ba9aea69d258f9ea890d8abb7cb
Aimer Neige <aimer.neige.soft@gmail.com>
2022-04-02 16:24:47 +0800

    update

---
+++ install.sh
@@ -0,0 +1,17 @@
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/CascadiaCode.zip -O /tmp/fonts/CascadiaCode.zip
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/UbuntuMono.zip -O /tmp/fonts/UbuntuMono.zip
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Ubuntu.zip -O /tmp/fonts/Ubuntu.zip
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip -O /tmp/fonts/FiraCode.zip
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip -O /tmp/fonts/JetBrainsMono.zip
+
+git clone https://github.com/laishulu/Sarasa-Mono-SC-Nerd.git /tmp/fonts/SarasaMonoScNerd
+
+git clone https://github.com/coderJianXun/Operator-Mono.git /tmp/fonts/OperatorMono
+
+git clone https://github.com/40huo/Patched-Fonts.git /tmp/fonts/patched
+
+git clone https://github.com/ajaybhatia/operator-mono-nerd-fonts.git /tmp/fonts/OperatorMonoNerd
